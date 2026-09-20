b64b0c15099acd36084241cc3eed65db4e564acd
voidensorincglguy-a11y <voidensorincglguy@gmail.com>
2026-09-20 21:09:33 +0800

    Add main function to print a message

--- Side.cpp
+++ Side.cpp
@@ -1,1 +1,8 @@
+#include <iostream> // Header file library for input/output functions
 
+int main() {
+    // std::cout prints the text inside the quotation marks
+    std::cout << "Why are you even here✌️🥀😭" << std::endl; 
+    
+    return 0; // Indicates the program finished successfully
+}
