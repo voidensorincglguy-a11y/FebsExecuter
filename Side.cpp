6a8445143a2b99a62cc5cd9177372c3fbdd89573
voidensorincglguy-a11y <voidensorincglguy@gmail.com>
2026-09-20 21:11:11 +0800

    Update output message in Side.cpp

--- Side.cpp
+++ Side.cpp
@@ -3,6 +3,6 @@
 int main() {
     // std::cout prints the text inside the quotation marks
     std::cout << "Why are you even here✌️🥀😭" << std::endl; 
-    
+    std::cout << "if you Actually go through this file then what is your purpose.";
     return 0; // Indicates the program finished successfully
 }
